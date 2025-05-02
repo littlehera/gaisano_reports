c0b19d37160761a6f68351c878ff857ec34aed10
Herabelle Villanueva <villanuevahera@gmail.com>
2025-05-02 13:41:23 +0800

    First Push

---
+++ gaisano_reports/gaisano_reports/doctype/clickhouse_settings/clickhouse_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Gaisano IT and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ClickhouseSettings(Document):
+	pass
