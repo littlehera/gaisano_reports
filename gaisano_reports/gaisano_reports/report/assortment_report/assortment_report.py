1728a2aadbda817f07f5507b06dbdb684723514a
Herabelle Villanueva <villanuevahera@gmail.com>
2025-06-04 15:10:48 +0800

    Add %Ctrl column to assortment report

--- gaisano_reports/gaisano_reports/report/assortment_report/assortment_report.py
+++ gaisano_reports/gaisano_reports/report/assortment_report/assortment_report.py
@@ -12,13 +12,15 @@ def execute(filters=None):
 	branch = filters.get('branch') if filters.get('branch') is not None else ""
 
 	data = get_data(from_date, to_date, branch)
+	data = get_ctrl(data)
 
 	columns = [
 		{"label": "Item", "fieldname": "item_name", "fieldtype": "Data", "width": 300},
 		{"label": "Category", "fieldname": "category", "fieldtype": "Data", "width": 180},
 		{"label": "Classification", "fieldname": "classification", "fieldtype": "Data", "width": 180},
 		{"label": "Subclass", "fieldname": "subclass", "fieldtype": "Data", "width": 180},
-		{"label": "Marketshare", "fieldname": "marketshare", "fieldtype": "Float", "Precision":2, "width": 180}
+		{"label": "Marketshare %", "fieldname": "marketshare", "fieldtype": "Float", "Precision":2, "width": 180},
+		{"label": "% Ctrl", "fieldname": "ctrl", "fieldtype": "Float", "Precision":2, "width": 180}
 	]
 
 	return columns, data
@@ -40,28 +42,30 @@ def get_data(from_date, to_date, branch):
 	if where_clause != "":
 		where_clause = "WHERE " + where_clause
 
+	q_total = """(select sum(amount) from greports.pos_data %s)"""%(where_clause)
 
-	q_item_list = """select cat.item_name, sum(pos.amount), cat.category, cat.classification, cat.subclass from greports.item_class_each cat
+	q_item_list = """select cat.item_name, 100*sum(pos.amount)/%s as marketshare, cat.category, cat.classification, cat.subclass from greports.item_class_each cat
 					join greports.pos_data pos on pos.barcode = cat.barcode %s group by cat.category, cat.classification, cat.subclass, cat.item_name
-					"""%(where_clause)
+					order by marketshare desc
+					"""%(q_total, where_clause)
 	item_list = client.query(q_item_list).result_rows
 
-	q_total = """select sum(amount) from greports.pos_data %s"""%(where_clause)
-	total_sales = client.query(q_total).result_rows
-
-	total_sales = float(total_sales[0][0])
-
 	for row in item_list:
-
-		item_amount = float(row[1])
-		marketshare = 100*(item_amount/total_sales)
 
 		data.append({
 			"item_name": row[0],
 			"category":row[2],
 			"classification":row[3],
 			"subclass": row[4],
-			"marketshare": marketshare
+			"marketshare": float(row[1])
 		})
 	
+	return data
+
+def get_ctrl(data):
+	for i,row in enumerate(data):
+		if i==0:
+			data[i]['ctrl'] = data[i]['marketshare']
+		else:
+			data[i]['ctrl'] = data[i-1]['ctrl']+data[i]['marketshare']
 	return data
